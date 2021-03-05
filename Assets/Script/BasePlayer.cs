89f48d21ca272cf25c8bb932d60d3466bf900029
FVending <dmitry@florarea.com>
2021-03-05 19:49:52 +0300

    изменил интерфейс

--- Assets/Script/BasePlayer.cs
+++ Assets/Script/BasePlayer.cs
@@ -46,25 +46,25 @@ public class BasePlayer : MonoBehaviour
 
         }
 
-        float HeightContent = Price.Count * Price.Count;
-        RectTransform rectTrans = Parent.transform as RectTransform;
-        rectTrans.offsetMin = new Vector2(0, - HeightContent);
-        string NameFile;
+        //float HeightContent = Price.Count * Price.Count;
+        ////RectTransform rectTrans = Parent.transform as RectTransform;
+        ////rectTrans.offsetMin = new Vector2(0, - HeightContent);
+        //string NameFile;
 
-        for (int i = 0; Price.Count > i; i++)
-        {
-            Button = Instantiate(ButtonPrifab, Button_Start.transform);
-            NameFile = Price[i]; //Берем из списка прайса для добавление к кнопке
-            NameButton = NameFile.Split('=').First(); //Берем название до равно
-            ValuePrice = NameFile.Split('=').Last();  //Берем название после равно
-            //Debug.Log(NameButton);
-            Button.GetComponentInChildren<Text>().text = NameButton; //Запись названия в кнопку
-            Button.GetComponentInChildren<Transform>().GetChild(1).GetComponent<Text>().text = ValuePrice;
-            //Button.GetComponentInChildren<Text>().text = Price[i];
-            Button.transform.parent = Parent.transform;
-            Button.transform.position = new Vector3(Button_Start.position.x, Button_Start.position.y - Step);
-            Step = Step + 50;
-        }
+        //for (int i = 0; Price.Count > i; i++)
+        //{
+        //    Button = Instantiate(ButtonPrifab, Button_Start.transform);
+        //    NameFile = Price[i]; //Берем из списка прайса для добавление к кнопке
+        //    NameButton = NameFile.Split('=').First(); //Берем название до равно
+        //    ValuePrice = NameFile.Split('=').Last();  //Берем название после равно
+        //    //Debug.Log(NameButton);
+        //    Button.GetComponentInChildren<Text>().text = NameButton; //Запись названия в кнопку
+        //    Button.GetComponentInChildren<Transform>().GetChild(1).GetComponent<Text>().text = ValuePrice;
+        //    //Button.GetComponentInChildren<Text>().text = Price[i];
+        //    Button.transform.parent = Parent.transform;
+        //    Button.transform.position = new Vector3(Button_Start.position.x, Button_Start.position.y - Step);
+        //    Step = Step + 50;
+        //}
         ReadPrice.Close();
     }
     
--- Assets/Script/Click_Button.cs
+++ Assets/Script/Click_Button.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Boo.Lang;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,22 +7,97 @@ public class Click_Button : MonoBehaviour
 {
     GameObject Camera;
     string Value;
-    string Name;
+    string SensorValue;
+    string Cell = "";
+    string DoubleCell = "";
+    bool Price_Bool = true;
+    List<string> Price_Value;
+  
+   
+
     void Start()
     {
-        Camera = GameObject.FindGameObjectWithTag("MainCamera"); // Ищем главную камеру на которой основные скрипты
+        Camera = GameObject.FindGameObjectWithTag("MainCamera"); // Ищем главную камеру на которой основные скрипты    
     }
 
-    public void Click_Butt()
+    void Read_Price()
     {
-        Value = gameObject.GetComponentInChildren<Transform>().GetChild(1).GetComponent<Text>().text; // присваиваем значение цены
-        Name = gameObject.GetComponentInChildren<Transform>().GetChild(0).GetComponent<Text>().text; // присваиваем имя
-        Camera.GetComponent<MDB>().Click_Price(Value, Name); // обращаемся к функции и передаем значение
+        Price_Value = new List<string>(Camera.GetComponent<BasePlayer>().Price);
+    }
+
+    void Input_Button()
+    {
+        if (Input.anyKeyDown)
+        {
+            if (Input.GetKeyDown("1")) Cell_NUM("1");
+            if (Input.GetKeyDown("2")) Cell_NUM("2");
+            if (Input.GetKeyDown("3")) Cell_NUM("3");
+            if (Input.GetKeyDown("4")) Cell_NUM("4");
+            if (Input.GetKeyDown("5")) Cell_NUM("5");
+            if (Input.GetKeyDown("6")) Cell_NUM("6");
+            if (Input.GetKeyDown("7")) Cell_NUM("7");
+            if (Input.GetKeyDown("8")) Cell_NUM("8");
+            if (Input.GetKeyDown("9")) Cell_NUM("9");
+            if (Input.GetKeyDown("0")) Cell_NUM("0");
+        }
+    }
+
+    void Cell_NUM(string Numer_Push_Btn)// тут проверяем номер ячейки. если номер из двух цыфр, то норм
+    {
+        if (DoubleCell.Length < 2) DoubleCell = DoubleCell + Numer_Push_Btn;
+        if (DoubleCell.Length == 2)
+        {
+            foreach (string Val in Price_Value) // Перебераем прайс
+            {
+                if (Val.Split('=').First() == DoubleCell)
+                {
+                    Value = Val.Split('=').Last();
+                    Cell = DoubleCell;
+                    Debug.Log("Цена " + Value + " Ячейка " + Cell);
+                    DoubleCell = "";
+                    Numer_Push_Btn = "";
+                    Debug.Log(DoubleCell.Length);
+                    Camera.GetComponent<MDB>().Click_Price(Value, Cell); // обращаемся к функции и передаем значение
+                }
+
+            }
+
+        }
+    }
+
+    //void OnGUI()
+    //{
+    //    GUIStyle styleTime = new GUIStyle();
+    //    styleTime.fontSize = 80;
+    //    styleTime.normal.textColor = Color.red;
+    //    if(int.Parse(Cell) < 11 || int.Parse(Cell) > 17)
+    //    GUI.Button(new Rect(200, 800, 200, 100), "Нет такой ячейки!", styleTime);        
+    //}
+
+
+
+    public void Click_Butt(string Sensor_Cell)
+    {     
+        foreach (string Val in Price_Value) // Перебераем прайс
+        {
+            if (Val.Split('=').First() == Sensor_Cell)
+            {
+                SensorValue = Val.Split('=').Last();
+                Debug.Log("Цена " + SensorValue + " Ячейка " + Sensor_Cell);
+                Camera.GetComponent<MDB>().Click_Price(SensorValue, Sensor_Cell); // обращаемся к функции и передаем значение
+            }          
+        }
     }
 
 
     void Update()
     {
-        
+        if (Price_Bool)// тут читаем прайс
+        {
+            Read_Price();
+            if (Price_Value.Count != 0) Price_Bool = false;
+        }
+
+        Input_Button();
     }
 }
